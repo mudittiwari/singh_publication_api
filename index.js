ceb882e13b25beb68f99dd30537638906b372c6b
mudittiwari <68576920+mudittiwari@users.noreply.github.com>
2023-05-06 23:45:22 +0530

    first commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 
 app.use(cors({
 //   origin: 'https://mudittiwari.github.io'
-origin:'http://singhpublication.in/'
+origin:'https://singhpublication.in'
 //   origin: 'http://localhost:3000'
 }));
 app.use("/api/user/",userRoute);
